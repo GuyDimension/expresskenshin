c9f46fe66401c77eb028986017fa382e9d7d55d5
kenshinverse <ricken.victoranthony@itbss.ac.id>
2024-06-23 18:16:09 +0700

    memperrbaiki landing page topup

--- library/database.js
+++ library/database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection([
+var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
 	password:'',
 	database:'Database_login'
-]);
+});
 connection.connect(function(error){
 	if(!!error) {
 		console.log(error);
